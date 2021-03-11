5df643722720be2e31f16feff3109a656f8b5be4
lstaborda <leticia.s.taborda@gmail.com>
2021-03-10 21:50:16 -0300

    Initial commit

---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import { Title } from './styles';
+
+const Dashboard: React.FC = () => {
+  return <Title>Explore respositórios no Github</Title>;
+};
+
+export default Dashboard;
